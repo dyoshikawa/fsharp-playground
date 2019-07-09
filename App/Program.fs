e1f4c579d0171ea3b946202202dfafbba4f86067
dyoshikawa <yoshikawada@crunchtimer.jp>
2019-07-09 09:08:07 +0900

    fix

--- App/Program.fs
+++ App/Program.fs
@@ -2,15 +2,14 @@
 
 [<EntryPoint>]
 let main _argv =
-    let a = stdin.ReadLine() |> int
-    let b = stdin.ReadLine() |> int
-    let c = stdin.ReadLine() |> int
-    let x = stdin.ReadLine() |> int
-
-    [for ia in [0 .. a] do
-        for ib in [0 .. b] do
-            for ic in [0 .. c] do
-                yield ia * 500 + ib * 100 + ic * 50]
-        |> List.sumBy (fun y -> if y = x then 1 else 0)
-        |> printfn "%d"           
+    let inputNums = stdin.ReadLine().Split(' ') |> Array.map int |> Array.toList
+    let N = inputNums.[0]
+    let A = inputNums.[1]
+    let B = inputNums.[2]
+    
+    [ for i in [1 .. N] do
+        let total = i.ToString().ToCharArray(0, i.ToString().Length) |> Array.toList |> List.map string |> List.map int |> List.sum
+        yield if total >= A && total <= B then i else 0]
+        |> List.sum
+        |> printfn "%A"
     0
