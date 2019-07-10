853203bed51fc4208aadff4c7df3e5d74c4c9d2d
dyoshikawa <yoshikawada@crunchtimer.jp>
2019-07-10 13:35:35 +0900

    fix

--- App/Program.fs
+++ App/Program.fs
@@ -1,15 +1,7 @@
 ﻿open System
+open Suave
 
 [<EntryPoint>]
 let main _argv =
-    let inputNums = stdin.ReadLine().Split(' ') |> Array.map int |> Array.toList
-    let N = inputNums.[0]
-    let A = inputNums.[1]
-    let B = inputNums.[2]
-    
-    [ for i in [1 .. N] do
-        let total = i.ToString().ToCharArray(0, i.ToString().Length) |> Array.toList |> List.map string |> List.map int |> List.sum
-        yield if total >= A && total <= B then i else 0]
-        |> List.sum
-        |> printfn "%A"
+    startWebServer defaultConfig (Successful.OK "Hello World!")
     0
